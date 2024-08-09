f0b93eb65e1ec529622c65001f51d5f710695708
Thomas Nordvig Hermansen <nordvigprivat@gmail.com>
2024-08-10 00:40:14 +0200

    Changed namespace to scoped namespace and removed unused usings

--- PersonalHub.Domain/Workspace/Entities/Space.cs
+++ PersonalHub.Domain/Workspace/Entities/Space.cs
@@ -1,12 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace PersonalHub.Domain.Workspace.Entities;
 
-namespace PersonalHub.Domain.Workspace.Entities
+public class Space
 {
-    public class Space
-    {
-    }
 }
