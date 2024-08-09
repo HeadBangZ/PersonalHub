fdde6f163f06e661f0a5b05e3e0913f2ec55f34f
Thomas Nordvig Hermansen <nordvigprivat@gmail.com>
2024-08-10 00:39:00 +0200

    Added Space and Epic classes

---
+++ PersonalHub.Domain/Workspace/Entities/Epic.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonalHub.Domain.Workspace.Entities
+{
+    public class Epic
+    {
+    }
+}
