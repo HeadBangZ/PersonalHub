79d3a16c743f332602d857342f6807da1987fc40
Thomas Nordvig Hermansen <nordvigprivat@gmail.com>
2024-08-19 05:05:00 +0200

    Added entity Ids (Strongly Typed

---
+++ PersonalHub.Domain/Feedback/ValueObjects/CommentId.cs
@@ -0,0 +1,7 @@
+﻿namespace PersonalHub.Domain.Feedback.ValueObjects;
+
+public readonly record struct CommentId(Guid Id)
+{
+    public static CommentId Empty => new(Guid.Empty);
+    public static CommentId NewEntityId() => new(Guid.NewGuid());
+}
---
+++ PersonalHub.Domain/Workspace/ValueObjects/EpicId.cs
@@ -0,0 +1,7 @@
+﻿namespace PersonalHub.Domain.Workspace.ValueObjects;
+
+public readonly record struct EpicId(Guid Id)
+{
+    public static EpicId Empty => new(Guid.Empty);
+    public static EpicId NewEntityId() => new(Guid.NewGuid());
+}
