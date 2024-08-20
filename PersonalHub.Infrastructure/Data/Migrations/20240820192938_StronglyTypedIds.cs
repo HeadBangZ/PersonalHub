afe72cd60bf56771feef4f9378d28239b4040ca2
Thomas Nordvig Hermansen <nordvigprivat@gmail.com>
2024-08-20 21:35:50 +0200

    Updated db conversion of BugId to Guid Id

---
+++ PersonalHub.Infrastructure/Data/Migrations/20240820192938_StronglyTypedIds.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PersonalHub.Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class StronglyTypedIds : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "71f56ec4-d567-4f00-99e5-451af5d4f017");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Features",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldMaxLength: 100);
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "18508d72-4abd-40b8-897a-0d88abbc8053", null, "User", "USER" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "18508d72-4abd-40b8-897a-0d88abbc8053");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Features",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldMaxLength: 100,
+                oldNullable: true);
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "71f56ec4-d567-4f00-99e5-451af5d4f017", null, "User", "USER" });
+        }
+    }
+}
