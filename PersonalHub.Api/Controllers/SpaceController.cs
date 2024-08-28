1854e1fb87c7a44026b6f9aa053c3d7201866eca
Thomas Nordvig Hermansen <nordvigprivat@gmail.com>
2024-08-28 14:26:32 +0200

    Created space controller

---
+++ PersonalHub.Api/Controllers/SpaceController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PersonalHub.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SpaceController : ControllerBase
+    {
+    }
+}
