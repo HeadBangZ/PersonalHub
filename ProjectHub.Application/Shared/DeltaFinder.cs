7d077a64e49084626d2308ffb733849e939fba29
Thomas Nordvig Hermansen <nordvigprivat@gmail.com>
2024-09-07 11:12:24 +0200

    Changed how I update entities

---
+++ ProjectHub.Application/Shared/DeltaFinder.cs
@@ -0,0 +1,35 @@
+﻿namespace ProjectHub.Application.Shared;
+
+public sealed class DeltaFinder
+{
+    public static Dictionary<string, object> GetChangedProperties<TSource, TTarget>(TSource source, TTarget target)
+    {
+        var changes = new Dictionary<string, object>();
+
+        var sourceProperties = typeof(TSource).GetProperties();
+        var targetProperties = typeof(TTarget).GetProperties();
+
+        foreach (var sourceProperty in sourceProperties)
+        {
+            if (sourceProperty.Name == "Id")
+            {
+                continue;
+            }
+
+            var targetProperty = targetProperties.FirstOrDefault(p => p.Name == sourceProperty.Name);
+
+            if (targetProperty != null && targetProperty.CanRead && sourceProperty.CanRead)
+            {
+                var sourceValue = sourceProperty.GetValue(source);
+                var targetValue = targetProperty.GetValue(target);
+
+                if (sourceValue != null && !sourceValue.Equals(targetValue))
+                {
+                    changes.Add(sourceProperty.Name, sourceValue);
+                }
+            }
+        }
+
+        return changes;
+    }
+}
