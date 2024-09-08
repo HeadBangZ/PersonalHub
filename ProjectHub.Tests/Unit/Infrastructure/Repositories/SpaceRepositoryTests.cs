51e3ee23581e8ee2c356008d3a85644411ecdb72
Thomas Nordvig Hermansen <nordvigprivat@gmail.com>
2024-09-08 19:40:59 +0200

    Changed it to a factory for the context, and added IAsyncLifetime to initialize adn teardown after each test

--- ProjectHub.Tests/Unit/Infrastructure/Repositories/SpaceRepositoryTests.cs
+++ ProjectHub.Tests/Unit/Infrastructure/Repositories/SpaceRepositoryTests.cs
@@ -1,44 +1,53 @@
-﻿using ProjectHub.Domain.Workspace.Entities;
+﻿using Microsoft.EntityFrameworkCore;
+using ProjectHub.Domain.Workspace.Entities;
 using ProjectHub.Domain.Workspace.Enums;
+using ProjectHub.Domain.Workspace.ValueObjects;
 using ProjectHub.Infrastructure.Data.Contexts;
 using ProjectHub.Infrastructure.Repositories;
 using ProjectHub.Tests.Unit.Mocks;
+using System.ComponentModel.DataAnnotations;
 
 namespace ProjectHub.Tests.Unit.Infrastructure.Repositories
 {
-    public class SpaceRepositoryTests
+    public class SpaceRepositoryTests : IAsyncLifetime
     {
         private readonly SpaceRepository _spaceRepository;
         private readonly ProjectHubDbContext _context;
 
         public SpaceRepositoryTests()
         {
-            _context = MockDbContextSingleton.Instance.DbContext;
-
+            _context = MockDbContextFactory.CreateDbContext();
             _spaceRepository = new SpaceRepository(_context);
         }
 
-        private static List<Space> GetTestSpaces()
+        private async Task SeedDataAsync()
         {
             var spaces = new List<Space>()
             {
                 new Space("Space 1", "Description Space 1", ProgressState.Completed, new List<Section>()),
                 new Space("Space 2", "Description Space 2", ProgressState.NotStarted, new List<Section>()),
                 new Space("Space 3", "Description Space 3", ProgressState.InProgress, new List<Section>()),
-                new Space("Space 4", "Description Space 4", ProgressState.NotStarted, new List<Section>())
+                new Space("Space 4", "Description Space 4", ProgressState.NotStarted, new List<Section>()),
             };
 
-            return spaces;
+            await _context.Spaces.AddRangeAsync(spaces);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task InitializeAsync()
+        {
+            await SeedDataAsync();
+        }
+
+        public async Task DisposeAsync()
+        {
+            _context.Spaces.RemoveRange(_context.Spaces);
+            await _context.SaveChangesAsync();
         }
 
         [Fact]
         public async Task GetAllAsync_Successfully()
         {
-            var spaces = GetTestSpaces();
-
-            await _context.Spaces.AddRangeAsync(spaces);
-            await _context.SaveChangesAsync();
-
             var result = await _spaceRepository.GetAllAsync();
 
             Assert.Equal(4, result.Count);
@@ -46,6 +55,32 @@ namespace ProjectHub.Tests.Unit.Infrastructure.Repositories
             Assert.Empty(result[1].Sections);
             Assert.Equal(ProgressState.InProgress, result[2].State);
             Assert.Equal("Description Space 4", result[3].Description);
+        }
+
+        [Fact]
+        public async Task GetById_Failed()
+        {
+            var result = await _spaceRepository.GetAsync(SpaceId.Empty);
+
+            Assert.Null(result);
+        }
+
+        [Fact]
+        public async Task GetById_Successfully()
+        {
+            var space = new Space("Space X", "Description Space X");
+
+            await _context.AddAsync(space);
+            await _context.SaveChangesAsync();
+
+            var result = await _spaceRepository.GetAsync(space.Id);
+
+            Assert.NotNull(result);
+            Assert.Equal(typeof(Space), result.GetType());
+            Assert.NotEqual(Guid.Empty, result.Id.Id);
+            Assert.Equal("Space X", result.Name);
+            Assert.Equal("Description Space X", result.Description);
+            Assert.Equal(ProgressState.NotStarted, result.State);
         }
     }
 }
---
+++ ProjectHub.Tests/Unit/Mocks/MockDbContextFactory.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using NSubstitute;
+using ProjectHub.Infrastructure.Data.Contexts;
+
+namespace ProjectHub.Tests.Unit.Mocks;
+
+public sealed class MockDbContextFactory
+{
+    public static ProjectHubDbContext CreateDbContext()
+    {
+        var mockConfiguration = Substitute.For<IConfiguration>();
+
+        var options = new DbContextOptionsBuilder<ProjectHubDbContext>()
+            .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+            .Options;
+
+        return new ProjectHubDbContext(options, mockConfiguration);
+    }
+}
