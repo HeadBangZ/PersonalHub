91eb82cdc3bfb854bb928e8b24a2e92e5d86e930
Thomas Nordvig Hermansen <nordvigprivat@gmail.com>
2024-09-19 17:36:48 +0200

    Started integration tests

---
+++ ProjectHub.Tests/Integration/Api/SpaceControllerTests.cs
@@ -0,0 +1,37 @@
+﻿using Newtonsoft.Json;
+using ProjectHub.Api;
+using ProjectHub.Application.DTOs.SpaceDtos;
+using ProjectHub.Domain.Workspace.Entities;
+using ProjectHub.Tests.Integration.Mocks;
+using System.Net;
+using System.Text;
+
+namespace ProjectHub.Tests.Integration.Api;
+
+public class SpaceControllerTests : IClassFixture<ApiWebApplicationFactory<Program>>
+{
+    private readonly HttpClient _client;
+
+    public SpaceControllerTests(ApiWebApplicationFactory<Program> factory)
+    {
+        _client = factory.CreateClient();
+    }
+
+    [Fact]
+    public async Task PostSpace_ShouldReturnCreated()
+    {
+        var request = new CreateSpaceDtoRequest("Test Space", "Test Description");
+
+        var json = new StringContent(JsonConvert.SerializeObject(request), Encoding.UTF8, "application/json");
+
+        var response = await _client.PostAsync("/api/spaces", json);
+
+        Assert.Equal(HttpStatusCode.Created, response.StatusCode);
+
+        var reponseContent = await response.Content.ReadAsStringAsync();
+        var created = JsonConvert.DeserializeObject<SpaceDtoResponse>(reponseContent);
+
+        Assert.NotNull(created);
+        Assert.Equal(request.Name, created.Name);
+    }
+}
