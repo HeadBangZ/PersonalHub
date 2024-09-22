4ae3f4c5e1e0ee6304c14117b67bacb26b33cfb6
Thomas Nordvig Hermansen <nordvigprivat@gmail.com>
2024-09-22 04:09:16 +0200

    Did some refactoring regarding my mock in integration test

--- ProjectHub.Tests/Integration/Api/SpaceControllerTests.cs
+++ ProjectHub.Tests/Integration/Api/SpaceControllerTests.cs
@@ -1,7 +1,8 @@
 ﻿using Newtonsoft.Json;
 using ProjectHub.Api;
 using ProjectHub.Application.DTOs.SpaceDtos;
-using ProjectHub.Tests.Integration.Mocks;
+using ProjectHub.Tests.Integration.Suite;
+using ProjectHub.Tests.Integration.Suite.Mocks;
 using System.Net;
 using System.Text;
 using System.Text.Json;
@@ -9,28 +10,28 @@ using System.Text.Json.Serialization;
 
 namespace ProjectHub.Tests.Integration.Api;
 
-public class SpaceControllerTests : IClassFixture<ApiWebApplicationFactory<Program>>
+public class SpaceControllerTests : ApiIntegrationTestFixture
 {
-    private readonly HttpClient _client;
-
-    public SpaceControllerTests(ApiWebApplicationFactory<Program> factory)
+    public SpaceControllerTests()
     {
-        _client = factory.CreateClient();
     }
 
     [Fact]
     public async Task PostSpace_ShouldReturnCreated()
     {
-        // Testing if it works with Newtonsoft
         var request = new CreateSpaceDtoRequest("Test Space", "Test Description");
         var json = new StringContent(JsonConvert.SerializeObject(request), Encoding.UTF8, "application/json");
 
-        _client.DefaultRequestHeaders.Accept.Clear();
-        _client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
-
-        var response = await _client.PostAsync("/api/spaces", json);
+        var response = await HttpClient.PostAsync("/api/spaces", json);
 
         Assert.Equal(HttpStatusCode.Created, response.StatusCode);
+
+        //_client.DefaultRequestHeaders.Accept.Clear();
+        //_client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+
+        //var response = await _client.PostAsync();
+
+        //Assert.Equal(HttpStatusCode.Created, response.StatusCode);
 
         //var responseContent = await response.Content.ReadAsStringAsync();
         //var created = JsonConvert.DeserializeObject<SpaceDtoResponse>(responseContent);
---
+++ ProjectHub.Tests/Integration/Suite/Mocks/ApiWebApplicationFactory.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using ProjectHub.Api;
+using ProjectHub.Infrastructure.Data.Contexts;
+
+namespace ProjectHub.Tests.Integration.Suite.Mocks;
+
+public class ApiWebApplicationFactory : WebApplicationFactory<Program>
+{
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.ConfigureServices(ReplaceDbContextWithInMemoryDb);
+    }
+
+    private static void ReplaceDbContextWithInMemoryDb(IServiceCollection services)
+    {
+        var existingDbContextRegistration = services.SingleOrDefault(d => d.ServiceType == typeof(ProjectHubDbContext));
+
+        if (existingDbContextRegistration != null)
+        {
+            services.Remove(existingDbContextRegistration);
+        }
+
+        var connectionStringBuilder = new SqliteConnectionStringBuilder { DataSource = "sql_lite_tests" };
+        var connectionString = connectionStringBuilder.ToString();
+        services.AddDbContext<ProjectHubDbContext>(options => options.UseSqlite(connectionString));
+    }
+
+    public ProjectHubDbContext CreateDbContext()
+    {
+        var dbContext = Services.CreateScope().ServiceProvider.GetService<ProjectHubDbContext>()!;
+        dbContext.Database.EnsureCreated();
+
+        return dbContext;
+    }
+}
