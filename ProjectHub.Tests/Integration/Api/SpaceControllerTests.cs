8214809d78646c0030b389d28a72db882c4560d2
Thomas Nordvig Hermansen <nordvigprivat@gmail.com>
2024-09-22 13:49:29 +0200

    Removed 2 files, and changed the ApiWebApplicationFactory, changed the Program.cs a little and fixed tests

--- ProjectHub.Tests/Integration/Api/SpaceControllerTests.cs
+++ ProjectHub.Tests/Integration/Api/SpaceControllerTests.cs
@@ -1,30 +1,56 @@
-﻿using Newtonsoft.Json;
-using ProjectHub.Api;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Writers;
+using Newtonsoft.Json;
 using ProjectHub.Application.DTOs.SpaceDtos;
-using ProjectHub.Tests.Integration.Suite;
+using ProjectHub.Infrastructure.Data.Contexts;
 using ProjectHub.Tests.Integration.Suite.Mocks;
 using System.Net;
 using System.Text;
-using System.Text.Json;
-using System.Text.Json.Serialization;
 
 namespace ProjectHub.Tests.Integration.Api;
 
-public class SpaceControllerTests : ApiIntegrationTestFixture
+public class SpaceControllerTests : IDisposable
 {
+    private ApiWebApplicationFactory _factory;
+    private readonly HttpClient _client;
+
     public SpaceControllerTests()
     {
+        _factory = new ApiWebApplicationFactory();
+        _client = _factory.CreateClient();
     }
 
-    //[Fact]
-    //public async Task PostSpace_ShouldReturnCreated()
-    //{
-    //    var request = new CreateSpaceDtoRequest("Test Space", "Test Description");
-    //    var json = new StringContent(JsonConvert.SerializeObject(request), Encoding.UTF8, "application/json");
+    [Fact]
+    public async Task Get_Space_ShoudlReturnOk()
+    {
+        var response = await _client.GetAsync("api/spaces");
 
-    //    var response = await HttpClient.PostAsync("/api/spaces", json);
+        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+    }
 
-    //    Assert.Equal(HttpStatusCode.Created, response.StatusCode);
-    //}
+    [Fact]
+    public async Task PostSpace_ShouldReturnCreated()
+    {
+
+        var request = new CreateSpaceDtoRequest("Test Space", "Test Description");
+        var json = new StringContent(JsonConvert.SerializeObject(request), Encoding.UTF8, "application/json");
+
+        var response = await _client.PostAsync("/api/spaces", json);
+
+        Assert.Equal(HttpStatusCode.Created, response.StatusCode);
+    }
+
+    public void Dispose()
+    {
+        using (var scope = _factory.Services.CreateScope())
+        {
+            var db = scope.ServiceProvider.GetRequiredService<ProjectHubDbContext>();
+            db.Database.EnsureDeleted();
+        }
+
+        _client.Dispose();
+        _factory.Dispose();
+    }
 }
+
 
--- ProjectHub.Tests/Integration/Suite/Mocks/ApiWebApplicationFactory.cs
+++ ProjectHub.Tests/Integration/Suite/Mocks/ApiWebApplicationFactory.cs
@@ -1,12 +1,10 @@
 ﻿using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
-using Microsoft.Data.Sqlite;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using ProjectHub.Api;
 using ProjectHub.Infrastructure.Data.Contexts;
-using System.Data.Common;
+
 
 namespace ProjectHub.Tests.Integration.Suite.Mocks;
 
@@ -16,46 +14,25 @@ public class ApiWebApplicationFactory : WebApplicationFactory<Program>
     {
         builder.ConfigureServices(services =>
         {
-            var descriptor = services.SingleOrDefault(d => d.ServiceType == typeof(ProjectHubDbContext));
+            var descriptor = services.SingleOrDefault(d => d.ServiceType == typeof(DbContextOptions<ProjectHubDbContext>));
+
             if (descriptor != null)
             {
                 services.Remove(descriptor);
             }
 
-            services.AddSingleton<DbConnection>(container =>
+            services.AddDbContext<ProjectHubDbContext>(options =>
             {
-                var connection = new SqliteConnection("DataSource=:memory:");
-                connection.Open();
-
-                return connection;
-            });
-
-            services.AddDbContext<ProjectHubDbContext>((container, options) =>
-            {
-                var connection = container.GetRequiredService<DbConnection>();
-                options.UseSqlite(connection);
+                options.UseInMemoryDatabase("TestDatabase");
             });
 
             var sp = services.BuildServiceProvider();
 
-            var scope = sp.CreateScope();
-            var appContext = scope.ServiceProvider.GetRequiredService<ProjectHubDbContext>();
-
-            try
+            using (var scope = sp.CreateScope())
             {
-                appContext.Database.EnsureCreated();
-            }
-            catch (Exception)
-            {
-                throw;
+                var db = scope.ServiceProvider.GetRequiredService<ProjectHubDbContext>();
+                db.Database.EnsureCreated();
             }
         });
-    }
-
-    public ProjectHubDbContext CreateDbContext()
-    {
-        var dbContext = Services.CreateScope().ServiceProvider.GetRequiredService<ProjectHubDbContext>();
-        dbContext.Database.EnsureCreated();
-        return dbContext;
     }
 }
